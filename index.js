a69929e61b9aa93cafdeba2b71b444ab8bb0ffe5
Rebekah Zhou <rebekahzhou@gmail.com>
2021-12-22 15:35:06 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(stringArray, eventName) {
+    const msgArray = [];
+    for (i=0; i<stringArray.length; i++) {
+        msgArray[i]=`Thank you, ${stringArray[i]}, for the awesome ${eventName} gift!`;
+    }
+    return msgArray;
+}
+
+console.log(writeCards(gifts, 'birthday'));
+
+function countDown(n) {
+    while(n>=0) {
+        console.log(n);
+        n--;
+    }
+}
+countDown(4);
